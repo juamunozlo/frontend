68d02e37251ca50376aea6e2a2700e70f3697055
Nelson David Aristizabal Amaya <ndaristizabala@unal.edu.co>
2023-10-13 21:04:19 -0500

    fix

--- src/home/components/Location.jsx
+++ src/home/components/Location.jsx
@@ -1,5 +1,4 @@
 import {Card, CardContent, CardMedia, Typography} from "@mui/material";
-import GoogleMaps from "simple-react-google-maps";
 
 export default function Location() {
     return (
@@ -15,17 +14,8 @@ export default function Location() {
                         <Typography align = "left">Fondo de bikini, Ocenao Pacifico</Typography>
                     </CardContent>
                 </div>
-                <GoogleMaps
-                    apiKey={"AIzaSyAIoaqD6zupornIMbdYcAfDaTSHjAjFWJ4"}
-                    style={{ height: "400px", width: "500px" }}
-                    zoom={12}
-                    center={{
-                        lat:11.606680364492856, lng: 165.37717861779691
-                    }}
-                    markers={[
-                        {lat:11.606680364492856, lng: 165.37717861779691}
-                    ]}
-                />
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d500359.65269393293!2d165.0154321287808!3d11.547569057460512!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x644c2180a24fadbf%3A0x4c3f21ce9753a027!2sAtol%C3%B3n%20Bikini!5e0!3m2!1ses-419!2sco!4v1697248865356!5m2!1ses-419!2sco" width="600" height="450" style={{ border: 0 }} allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+                
             </Card>
         </div>
     );
